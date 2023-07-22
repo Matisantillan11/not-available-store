721ddfab2572d3c8488ca48e3822043d23e0a602
matisantillan11 <matias1.santillan@gmail.com>
2023-07-22 02:44:14 -0300

    fix: add header shadow

--- src/modules/common/components/layout/header.component.tsx
+++ src/modules/common/components/layout/header.component.tsx
@@ -3,7 +3,7 @@ import { Hamburguer } from './hamburguer.component'
 import { HeaderList } from './list/header-list.component'
 export const Header = () => {
 	return (
-		<div className='pl-[15px] pr-[15px] w-full flex justify-between items-center h-[45px] bg-primary-white'>
+		<div className='pl-[15px] pr-[15px] w-full flex justify-between items-center h-[65px] bg-transparent shadow-md'>
 			<Hamburguer />
 			<p className=' text-[18px] font-bold'>NOT AVAILABLE</p>
 			<div className='lg:absolute right-5 h-full flex justify-center items-center'>
